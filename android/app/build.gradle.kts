5f5c2157ec3e9b83495b000e4eb19b547c496569
Nipun Kumar <nipun.kumar@mayadataprivacy.eu>
2025-10-31 11:24:03 +0530

    Google login flow phase 1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.FileInputStream
+import java.util.Properties
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -6,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.nmsc_music"
+    namespace = "com.nipunapps.nmsc_todo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.nmsc_music"
+        applicationId = "com.nipunapps.nmsc_todo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -30,11 +32,39 @@ android {
         versionName = flutter.versionName
     }
 
+    // Load keystore properties
+    val keystoreProperties = Properties().apply {
+        load(FileInputStream(rootProject.file("keystore.properties")))
+    }
+
+    signingConfigs {
+
+        getByName("debug") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
+
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+        debug {
             signingConfig = signingConfigs.getByName("debug")
+        }
+        release {
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
--- lib/presentation/screens/auth/login_page.dart
+++ lib/presentation/screens/auth/login_page.dart
@@ -1,12 +1,21 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:nmsc_todo/core/ui/size.dart';
 import 'package:nmsc_todo/presentation/components/auth_logo.dart';
+import 'package:nmsc_todo/presentation/components/buttons/nmsc_primary_button.dart';
 import 'package:nmsc_todo/presentation/components/buttons/nmsc_text_button.dart';
 import 'package:nmsc_todo/presentation/components/google_login.dart';
 import 'package:nmsc_todo/presentation/components/nmsc_et_field.dart';
-import 'package:nmsc_todo/presentation/components/buttons/nmsc_primary_button.dart';
 import 'package:nmsc_todo/presentation/notifier/login_notifier.dart';
 import 'package:provider/provider.dart';
+
+const List<String> scopes = <String>[
+  'https://www.googleapis.com/auth/contacts.readonly',
+];
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -27,6 +36,69 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  String _contactText = '';
+  String _errorMessage = '';
+
+  Future<void> _handleAuthenticationEvent(
+    GoogleSignInAuthenticationEvent event,
+  ) async {
+    // #docregion CheckAuthorization
+    final GoogleSignInAccount? user = // ...
+        // #enddocregion CheckAuthorization
+        switch (event) {
+          GoogleSignInAuthenticationEventSignIn() => event.user,
+          GoogleSignInAuthenticationEventSignOut() => null,
+        };
+    if (kDebugMode) {
+      print(
+        "Google user${user == null ? ' signed out' : ' signed in: ${user.email}'}",
+      );
+    }
+    // Check for existing authorization.
+    // #docregion CheckAuthorization
+    final GoogleSignInClientAuthorization? authorization = await user
+        ?.authorizationClient
+        .authorizationForScopes(scopes);
+    // #enddocregion CheckAuthorization
+  }
+
+  Future<void> _handleAuthenticationError(Object e) async {
+    setState(() {
+      _errorMessage = e is GoogleSignInException
+          ? _errorMessageFromSignInException(e)
+          : 'Unknown error: $e';
+    });
+  }
+
+  String _errorMessageFromSignInException(GoogleSignInException e) {
+    // In practice, an application should likely have specific handling for most
+    // or all of the, but for simplicity this just handles cancel, and reports
+    // the rest as generic errors.
+    return switch (e.code) {
+      GoogleSignInExceptionCode.canceled => 'Sign in canceled',
+      _ => 'GoogleSignInException ${e.code}: ${e.description}',
+    };
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final GoogleSignIn signIn = GoogleSignIn.instance;
+    final clientId = dotenv.env["GOOGLE_CLIENT_ID"] ?? "";
+    final serverClientId = dotenv.env["GOOGLE_SERVER_CLIENT_ID"] ?? "";
+    unawaited(
+      signIn
+          .initialize(clientId: clientId, serverClientId: serverClientId)
+          .then((_) {
+            signIn.authenticationEvents
+                .listen(_handleAuthenticationEvent)
+                .onError(_handleAuthenticationError);
+
+            signIn.attemptLightweightAuthentication();
+          }),
+    );
+  }
 
   @override
   void dispose() {
@@ -107,8 +179,26 @@ class _LoginPageState extends State<LoginPage> {
                       NMSCTextButton(text: "Register", onClick: () => {}),
                     ],
                   ),
+                  if (GoogleSignIn.instance.supportsAuthenticate())
+                    const SizedBox(height: AppSize.x_4),
+                  if (GoogleSignIn.instance.supportsAuthenticate())
+                    GoogleLogin(
+                      onClick: () async {
+                        try {
+                          await GoogleSignIn.instance.authenticate();
+                        } catch (e) {
+                          _errorMessage = e.toString();
+                        }
+                      },
+                    ),
                   const SizedBox(height: AppSize.x_4),
-                  GoogleLogin(onClick: () {}),
+                  if (_errorMessage.isNotEmpty)
+                    Text(
+                      _errorMessage,
+                      style: textTheme.bodyMedium?.copyWith(color: Colors.red),
+                    ),
+                  if (_contactText.isNotEmpty)
+                    Text(_contactText, style: textTheme.bodyMedium),
                 ],
               ),
             ),
